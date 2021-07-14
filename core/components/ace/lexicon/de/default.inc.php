a7915e6ed0f5597374ebbce901a69b2e411ae91d
Ruslan-Aleev <aleev.insmile@gmail.com>
2021-07-14 13:34:08 +0400

    Corrected lexicons

--- core/components/ace/lexicon/de/default.inc.php
+++ core/components/ace/lexicon/de/default.inc.php
@@ -12,16 +12,16 @@ $_lang['ace'] = 'Ace';
 $_lang['ui_ace.case_sensitive']      = 'Gro&szlig;-/Kleinschreibung beachten';
 $_lang['ui_ace.close']               = 'Schlie&szlig;en';
 $_lang['ui_ace.find']                = 'Suchen';
+$_lang['ui_ace.find_all']            = 'Alle';
 $_lang['ui_ace.go']                  = 'Los';
 $_lang['ui_ace.goto_line']           = 'Gehe zu Zeile';
-$_lang['ui_ace.replace']             = 'Ersetzen';
-$_lang['ui_ace.replace_with']        = 'Ersetzen durch';
-$_lang['ui_ace.replace_all']         = 'Alle ersetzen';
-$_lang['ui_ace.message_replaced']    = 'Ersetzt: [[+count]].';
-$_lang['ui_ace.search_wrap']         = 'Am Anfang fortsetzen';
-$_lang['ui_ace.whole_word']          = 'Ganze W&ouml;rter';
 $_lang['ui_ace.maximize']            = 'Maximize';
 $_lang['ui_ace.minimize']            = 'Minimize';
+$_lang['ui_ace.reg_exp']             = 'Regulären Ausdruck';
+$_lang['ui_ace.replace']             = 'Ersetzen';
+$_lang['ui_ace.replace_all']         = 'Alle';
+$_lang['ui_ace.replace_with']        = 'Ersetzen durch';
+$_lang['ui_ace.whole_word']          = 'Ganze W&ouml;rter';
 
 $_lang['area_general']               = 'Allgemeine Einstellungen';
 
