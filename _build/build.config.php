f061e8eeae1154d67ff4c8fc63c771074795bc4a
Bochkarev Ivan <ivanx86@gmail.com>
2021-08-25 22:56:08 +0600

    Version 1.9.2

--- _build/build.config.php
+++ _build/build.config.php
@@ -1,18 +1,16 @@
 <?php
 
-define('PKG_NAME','Ace');
-define('PKG_NAMESPACE','ace');
-define('PKG_VERSION','1.9.1');
-define('PKG_RELEASE','pl');
+define('PKG_NAME', 'Ace');
+define('PKG_NAMESPACE', 'ace');
+define('PKG_VERSION', '1.9.2');
+define('PKG_RELEASE', 'pl');
 define('PKG_AUTO_INSTALL', true);
 
 if (isset($_SERVER['MODX_BASE_PATH'])) {
 	define('MODX_BASE_PATH', $_SERVER['MODX_BASE_PATH']);
-}
-elseif (file_exists(dirname(dirname(dirname(__FILE__))) . '/core')) {
+} elseif (file_exists(dirname(dirname(dirname(__FILE__))) . '/core')) {
 	define('MODX_BASE_PATH', dirname(dirname(dirname(__FILE__))) . '/');
-}
-else {
+} else {
 	define('MODX_BASE_PATH', dirname(dirname(dirname(dirname(__FILE__)))) . '/');
 }
 
@@ -21,7 +19,7 @@ define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
 define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
 define('MODX_ASSETS_PATH', MODX_BASE_PATH . 'assets/');
 
-define('MODX_BASE_URL','/');
+define('MODX_BASE_URL', '/');
 define('MODX_CORE_URL', MODX_BASE_URL . 'core/');
 define('MODX_MANAGER_URL', MODX_BASE_URL . 'manager/');
 define('MODX_CONNECTORS_URL', MODX_BASE_URL . 'connectors/');
