fedb886f1d64a0a54f5d3c6d48c8a7f03a695fc4
Ivan Bochkarev <ibochkarev@uma.tech>
2024-02-23 16:30:42 +0600

    feat: Update version

--- _build/build.config.php
+++ _build/build.config.php
@@ -2,7 +2,7 @@
 
 define('PKG_NAME', 'Ace');
 define('PKG_NAMESPACE', 'ace');
-define('PKG_VERSION', '1.9.3');
+define('PKG_VERSION', '1.9.4');
 define('PKG_RELEASE', 'pl');
 define('PKG_AUTO_INSTALL', true);
 
